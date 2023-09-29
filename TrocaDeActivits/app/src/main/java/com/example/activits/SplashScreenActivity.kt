fd73d127576540eb48f9c24212e1c8aac1bc7522
Savio Kane <savio.kanesk@gmail.com>
2023-09-29 11:22:16 -0300

    Activity C/ Handler

---
+++ TrocaDeActivits/app/src/main/java/com/example/activits/SplashScreenActivity.kt
@@ -0,0 +1,19 @@
+package com.example.activits
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+        },2900)
+    }
+}
